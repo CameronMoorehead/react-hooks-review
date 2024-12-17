4d13f957b6bb630162b9786957aaaee009691128
Cameron Moorehead <hello@cameronmoorehead.com>
2024-12-16 20:41:10 -0800

    save changes

---
+++ reaction/src/App.js
@@ -0,0 +1,17 @@
+import { useReducer } from "react";
+import reducer, { initialState } from './state/reducer';
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log('state', state);
+
+
+  return (
+    <div>
+      <h2>Reaction</h2>
+    </div>
+  );
+}
+
+export default App;
