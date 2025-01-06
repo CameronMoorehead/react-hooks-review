ed826315ec8770052545d0bde6670659468fa6d7
Cameron Moorehead <hello@cameronmoorehead.com>
2025-01-06 15:51:31 -0800

    save changes

--- home/src/tasks.js
+++ home/src/tasks.js
@@ -1,71 +1,122 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { v4 as uuid } from 'uuid';
 
+const initialTasksState = {
+  tasks: [],
+  completedTasks: []
+};
+
+const TYPES = {
+  ADD_TASK: 'ADD_TASK',
+  COMPLETE_TASK: 'COMPLETE_TASK',
+  DELETE_TASK: 'DELETE_TASK'
+};
+
+const tasksReducer = (state, action) => {
+  console.log('state', state, 'action', action);
+
+  switch(action.type) {
+    case TYPES.ADD_TASK:
+      return {
+        ...state,
+        tasks: [...state.tasks, action.task]
+      }
+    case TYPES.COMPLETE_TASK:
+      const { completedTask } = action;
+
+      return {
+        ...state,
+        completedTasks: [...state.completedTasks, completedTask],
+        tasks: state.tasks.filter(t => t.id !== completedTask.id)
+      }
+    case TYPES.DELETE_TASK:
+      return {
+        ...state,
+        completedTasks: state.completedTasks.filter(t => t.id !== action.task.id)
+      }
+    default:
+      return state;
+  }
+}
+
+const TASKS_STORAGE_KEY = 'TASKS_STORAGE_KEY';
+
+const storeTasks = (taskMap) => {
+  localStorage.setItem(
+    TASKS_STORAGE_KEY,
+    JSON.stringify(taskMap)
+  );
+}
+
+const readStoredTasks = () => {
+  const tasksMap = JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY));
+
+  return tasksMap ? tasksMap : initialTasksState;
+}
+
 function Tasks() {
-    const [taskText, setTaskText] = useState('');
-    const [tasks, setTasks] = useState([]);
-    const [completedTasks, setCompletedTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+  const storedTasks = readStoredTasks();
 
-    // setState used to merge new values into existing state object 
-    // if there were keys whos values had not changed, there would be
-    // no need to replace the existing values at those keys
-    
-    // The new setter functions on useState replace the entire value
-    // under the hood 
+  const [state, dispatch] = useReducer(tasksReducer, storedTasks);
+  const { tasks, completedTasks } = state;
 
-    const updateTaskText = event => {
-        setTaskText(event.target.value);
-    }
+  useEffect(() => {
+    storeTasks({ tasks, completedTasks });
+  });
 
-    const addTask = () => {
-        setTasks([ ...tasks, { taskText, id: uuid() } ])
-        setTaskText('');
-    }
+  const updateTaskText = event => {
+    setTaskText(event.target.value);
+  }
 
-    const completeTask = completedTask => () => {
-        setCompletedTasks([ ...completedTasks, completedTask ]);
-        setTasks(tasks.filter(task => task.id !== completedTask.id));
-    }
+  const addTask = () => {
+    dispatch({ type: TYPES.ADD_TASK, task: { taskText, id: uuid() } });
+  }
 
-    const deleteTask = deletedTask => () => {
-        setCompletedTasks(completedTasks.filter(completedTask => completedTask.id !== deletedTask.id));
-    }
+  const completeTask = completedTask => () => {
+    dispatch({ type: TYPES.COMPLETE_TASK, completedTask });
+  }
 
-    return (
-        <div>
-            <h3>Tasks</h3>
-            <div className="form">
-                <input value={ taskText } onChange={ updateTaskText } />
-                <button onClick={ addTask }>Add Task</button>
-            </div>
-            <div className="task-list">
-                {
-                    tasks.map(task => {
-                        const { id, taskText } = task;
-                        return (
-                            <div key={ id } onClick={ completeTask(task) }>
-                                { taskText }
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="completed-list">
-                {
-                    completedTasks.map(task => {
-                        const { id, taskText } = task;
+  const deleteTask = task => () => {
+    dispatch({ type: TYPES.DELETE_TASK, task });
+  }
 
-                        return (    
-                            <div key={ id }>
-                                { taskText }{' '}
-                                <span onClick={ deleteTask(task) } className="delete-task">x</span>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h3>Tasks</h3>
+      <div className='form'>
+        <input value={taskText} onChange={updateTaskText} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='task-list'>
+        {
+          tasks.map(task => {
+            const { id, taskText } = task;
+
+            return (
+              <div key={id} onClick={completeTask(task)}>
+                {taskText}
+              </div>
+            );
+          })
+        }
+      </div>
+      <div className='completed-list'>
+        {
+          completedTasks.map(task => {
+            const { id, taskText } = task;
+
+            return (
+              <div key={id}>
+                {taskText}{' '}
+                <span onClick={deleteTask(task)} className='delete-task'>x</span>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Tasks;
